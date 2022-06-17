34fdf56636b8703ec6a3f0d3bda002eb5d95438e
Skylyong <lyong_s@foxmail.com>
2022-06-17 18:01:16 +0800

    更新预处理和数据分析代码

--- back_tool.py
+++ back_tool.py
@@ -3,7 +3,14 @@ import  numpy as np
 import os
 import random
 
-def processing_data(compete_type_filename, data_filename):
+def processing_data(compete_type_filename, data_filename, n=None):
+    '''
+
+    :param compete_type_filename: 竞赛类型 '模式识别数据集' or '模式识别数据集'
+    :param data_filename:  数据集类型 'train' or 'validation'
+    :param n: 导入的样本数 None全部导入
+    :return:
+    '''
     file_names = []
     for root, dirs, files in os.walk(f'./{compete_type_filename}/{data_filename}'):
         for name in files:
@@ -21,11 +28,11 @@ def processing_data(compete_type_filename, data_filename):
         y_targets.append(file.split('/')[-2])
         X_seq = X_seq.reshape(-1).tolist()
         X_seqs.append(X_seq)
-        if idx > 100:break
+        if n is not None and idx >= n: break
     res_df = pd.DataFrame({'X_seqs':X_seqs, 'y_targets':y_targets}, index=None)
     res_df.sort_values(by=['y_targets'], inplace=True, ignore_index=True)
     res_df.to_pickle(f"./{compete_type_filename}/{data_filename}_data.pkl")
 
 
-processing_data('模式识别数据集', 'train')
-processing_data('模式识别数据集', 'validation')
+processing_data('模式识别数据集', 'train', n=100)
+processing_data('模式识别数据集', 'validation', n=100)
